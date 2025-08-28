b8a47311922dd161f01e6bfe227ad4b247eb1ab4
doralchan <dora.l.chan@gmail.com>
2025-08-28 11:14:43 -0700

    update tag styles

--- src/components/tag.jsx
+++ src/components/tag.jsx
@@ -11,9 +11,9 @@ const TagStyles = styled('div')`
   display: block;
   border-radius: var(--radius);
   font-size: 10px;
-  padding: var(--space-xs) var(--space-sm);
-  border: 1px solid var(--pink200);
-  color: var(--pink200);
+  padding: 2px var(--space-xs);
+  color: var(--white);
+  background-color: rgba(180, 80, 188, 0.5);
 `;
 
 export default Tag;
